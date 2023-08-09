642bf4a5e3970055629c066d44dabe0801ea5232
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2023-08-09 14:45:31 -0400

    Connecting to Mongobd and mongoose

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 let express = require("express");
-
 let app = express();
+let mongoose = require("mongoose");
+let Post = require("./models/post.model").Post;
+
+mongoose.connect("mongodb://127.0.0.1:27017/travels");
+
+let post1 = new Post({
+  id: "1",
+  title: "Eiffel Tower",
+  date: new Date(),
+  description: "Some desc",
+  text: "Some text",
+  country: "France",
+  imageURL: "/images/img-1.jpg",
+});
+post1.save().then(() => console.log("Saved."));
 
 app.use(express.static("public"));
 

7405f183d4c5d9e62bc1ca1888e5d3028f76eb58
yevgen-sydoruk <61704998+yevgen-sydoruk@users.noreply.github.com>
2023-08-14 12:01:12 -0400

    updating the port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let auth = require("./controllers/auth");
 
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb://127.0.0.1:27017/travels");
+mongoose.connect("mongodb+srv://drevil:MeLiOdAs2@mycluster.7ayi3g8.mongodb.net/travels");
 app.use(express.json());
 
 let imageStorage = multer.diskStorage({
@@ -59,4 +59,5 @@ app.get("/login", (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Listening on port 3000..."));
+let port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
